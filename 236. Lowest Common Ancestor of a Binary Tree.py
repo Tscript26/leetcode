c66fae29309822a7692c6182797de7cd13ad416d
zhuyilun <yilun.zhu@aminer.cn>
2021-09-15 10:46:34 +0800

    [236]

---
+++ 236. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,65 @@
+"""
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+
+Input: root = [3,5,1,6,2,0,8,None,None,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+
+Input: root = [3,5,1,6,2,0,8,None,None,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the tree.
+"""
+
+from src import *
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root.val == p.val or root.val == q.val:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and left.val is not None:
+            if right and right.val is not None:
+                return root
+            return left
+        return right
+
+
+root = [3, 5, 1, 6, 2, 0, 8, None, None, 7, 4]
+
+q = TreeNode(4)
+root = TreeNode().build(root)
+p = root.left
+# root = [1,2,3]
+# p = TreeNode(2)
+# q = TreeNode(3)
+# root = TreeNode().build(root)
+S = Solution()
+anw = S.lowestCommonAncestor(root, p, q)
+print(anw.val)
+# print(anw.bfs())
--- src/TreeNode.py
+++ src/TreeNode.py
@@ -9,31 +9,30 @@ class TreeNode:
         self.right = right
 
     @staticmethod
-    def build(ns):
-        nodes = copy(ns)
-        if not nodes:
-            return None
-        head = TreeNode(nodes.pop(0))
-        stack = [head]
-        no_left = False
-        while nodes:
-            now = stack.pop(0)
-            nxt = nodes.pop(0)
-            if not now:
-                nodes.pop(0)
-                continue
-            if not now.left and not no_left:
-                if nxt is None:
-                    no_left = True
-                now.left = TreeNode(nxt) if nxt is not None else None
-                stack.append(now)
-                stack.append(now.left)
-                continue
-            if not now.right:
-                now.right = TreeNode(nxt)
-                stack.append(now.right)
-            no_left = False
-        return head
+    def build(data):
+        if not data:
+            return []
+        # data = '['+','.join([str(node) if node is not None else 'None' for node in data]) + ']'
+        # dataList = data[1:-1].split(',')
+        dataList = [str(node) if node is not None else 'None' for node in data]
+        root = TreeNode(int(dataList[0]))
+        queue = deque([root])
+        i = 1
+        while queue:
+            node = queue.popleft()
+            if i >= len(dataList):
+                break
+            if dataList[i] != 'None':
+                node.left = TreeNode(int(dataList[i]))
+                queue.append(node.left)
+            i += 1
+            if i >= len(dataList):
+                break
+            if dataList[i] != 'None':
+                node.right = TreeNode(int(dataList[i]))
+                queue.append(node.right)
+            i += 1
+        return root
 
     def bfs(self):
         def parse(nodes):
@@ -57,7 +56,58 @@ class TreeNode:
         return result
 
 
+class TreeBuilder:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+        queue = deque([root])
+        res = []
+        while queue:
+            node = queue.popleft()
+            if node:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                res.append('None')
+        return '[' + ','.join(res) + ']'
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return []
+        dataList = data[1:-1].split(',')
+        root = TreeNode(int(dataList[0]))
+        queue = deque([root])
+        i = 1
+        while queue:
+            node = queue.popleft()
+            if i >= len(dataList):
+                break
+            if dataList[i] != 'None':
+                node.left = TreeNode(int(dataList[i]))
+                queue.append(node.left)
+            i += 1
+            if i >= len(dataList):
+                break
+            if dataList[i] != 'None':
+                node.right = TreeNode(int(dataList[i]))
+                queue.append(node.right)
+            i += 1
+        return root
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
